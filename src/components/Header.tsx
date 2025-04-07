9266d2ead7e7425d10f539047073286ac4323d68
facialjun <facialjun@gmail.com>
2025-04-07 23:02:04 +0900

    [wip] header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,20 @@
-import Link from 'next/link';
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="fixed w-full top-0 z-50 bg-white/95">
-      <nav className="max-w-[1200px] mx-auto px-6 py-4 flex justify-between items-center">
+    <header className="fixed w-full top-0 z-50 bg-white/95 font-serif">
+      <nav className="max-w-[1200px] mx-auto px-6 py-8 flex justify-center items-center">
         <Link href="/" className="flex items-center">
-          {/* Logo space if needed */}
+          <img
+            src="/assets/logo.png"
+            alt="Loqu"
+            className="h-8 w-auto max-w-[180px] object-contain"
+            width={120}
+            height={32}
+          />
         </Link>
       </nav>
     </header>
   );
-} 
+}
