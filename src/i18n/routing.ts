5d43e628f44010ea2bfaca74a913811f84af35f3
facialjun <facialjun@gmail.com>
2025-04-09 23:59:26 +0900

    create language translation

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,7 +2,19 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["en", "de"],
+  locales: [
+    "en",
+    "ko",
+    "ja",
+    "de",
+    "fr",
+    "es",
+    "it",
+    "pt",
+    "hi",
+    "zhtw",
+    "zhcn",
+  ],
 
   // Used when no locale matches
   defaultLocale: "en",
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,32 @@
 import createMiddleware from "next-intl/middleware";
-import { routing } from "./i18n/routing";
 
 // Middleware configuration
 export default createMiddleware({
-  ...routing,
+  // A list of all locales that are supported
+  locales: [
+    "en",
+    "ko",
+    "ja",
+    "de",
+    "fr",
+    "es",
+    "it",
+    "pt",
+    "hi",
+    "zhtw",
+    "zhcn"
+  ],
+
+  // Used when no locale matches
+  defaultLocale: "en",
+
   // Force locale prefix for all locales
-  localePrefix: "always"
+  localePrefix: "always",
 });
 
 export const config = {
   // Match all pathnames except for
   // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
   // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: ["/((?!api|trpc|_next|_vercel|.*\\..*).*)", "/"]
+  matcher: ["/((?!api|trpc|_next|_vercel|.*\\..*).*)", "/"],
 };
